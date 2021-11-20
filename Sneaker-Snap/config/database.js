5c74a4c10c288cc59819810475caaf65bb6d8b26
Daphny Barbosa <daphnyemily@gmail.com>
2021-11-19 21:23:00 -0500

    adding sneaker snap app

---
+++ Sneaker-Snap/config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://devdaph:tPshcQuYG0ltFxR9@cluster0.h8uhl.mongodb.net/sneakerSnap?retryWrites=true&w=majority', 
+    'dbName': 'sneakerSnap'
+};
+
+
+    // 'url' :  "mongodb+srv://karimhassan:mypassword@cluster0.byp1e.mongodb.net/InstaPrac?retryWrites=true&w=majority", 
+    // 'dbName': 'demo'
